ec3d4719c05829f24ee76b9c6843c7931520d302
hilmanmaulana1237 <1237050020@student.uinsgd.ac.id>
2026-01-21 03:17:10 +0700

    fix v1

--- app/Http/Controllers/UmkmController.php
+++ app/Http/Controllers/UmkmController.php
@@ -7,6 +7,7 @@ use App\Models\Product;
 use App\Models\UmkmStore;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 use App\Services\ImageService;
